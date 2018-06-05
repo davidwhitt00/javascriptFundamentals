399bf1a303cb5940ab330cdcfaeb25c01f9b08bd
David Whitt <davidwhitt00@gmail.com>
2018-06-05 11:01:16 -0400

    Initaial commit

---
+++ 1-grammerAndTypes/02-declarations.js
@@ -0,0 +1,12 @@
+//Variables
+
+var a = 1;
+var b = 2;
+
+//Declarations
+//Left Hand Side = var a
+//Right Hand Side = a = 1
+
+//Initialization = diffrent runs
+//var, let, and const
+//const can not change
---
+++ 1-grammerAndTypes/05-types.js
@@ -0,0 +1,48 @@
+// Types
+
+// Booleans
+var bool = true;
+console.log(bool);
+
+let noBool = false;
+console.log(noBool);
+
+//Null
+var empty = null;
+console.log(empty);
+
+//Numbers
+var degrees = 90
+console.log(" It is currently " + degrees + " degrees.");
+
+//Strings
+// Numbers vs. Strings
+let first = 1050 + 100;
+let secnond ="1050" + "100";
+
+console.log(first);
+console.log(secnond);
+
+var firstName = "Dave"
+var lastName = "Whitt"
+var houseNumber = 5508
+var street = "Floating Leaf Dr."
+var city = "Indinapolis"
+var state = "IN"
+var zip = 46237
+
+console.log("My name is " + firstName + " " + lastName + " I live on " + houseNumber + " " + street + " " + city + " " + state + " " + zip);
+
+console.log("My name is " + firstName,lastName + " I live on " + houseNumber,street,city,state,zip);
+
+let burritoNow = {
+    size: "large",
+    quantity: 4,
+    now:true};
+    console.log(burritoNow);
+
+    //Arrays
+    var burritos = ["large",4,true];
+    console.log(burritos);
+
+    
